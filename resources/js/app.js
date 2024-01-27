c609657103c7de6a3015da015ea435e1d114cc83
LakM <epmadushanka@gmail.com>
2024-01-27 11:35:50 +0530

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ import PrimeVue from 'primevue/config';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({el, App, props, plugin}) {
         return createApp({render: () => h(App, props)})
