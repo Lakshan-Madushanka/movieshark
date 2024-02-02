2dc5a2339fbf0140c81e0dfc5abd61681f57ee6e
LakM <epmadushanka@gmail.com>
2024-02-02 11:17:26 +0530

    Toggle movie from watch list feature

--- resources/js/Components/Movie/MovieTile.vue
+++ resources/js/Components/Movie/MovieTile.vue
@@ -1,19 +1,49 @@
 <script setup>
-import {ref} from "vue";
+import {onMounted, ref, watch} from "vue";
+import {useToggleMovieFromWatchList} from "@/Composables/useToggleMovieFromWatchList.js";
+import {useToast} from "primevue/usetoast";
 
 const props = defineProps({
     movie: {},
     widthClass: '',
     heightClass: '',
+    watchListIds: {
+        type: Object
+    },
+    showWatchListButton: {
+        type: Boolean,
+        default: true,
+    }
 });
 
+const toast = useToast();
+
+const watchListHas = ref();
+const {toggleFromWatchList, checkWatchListHas} = useToggleMovieFromWatchList(props.watchListIds);
+
+onMounted(() => {
+    watchListHas.value = checkWatchListHas(props.movie.id)
+})
+
 const showMovieInfoForId = ref(-100000);
+
 const setMovieInfoId = function (id) {
     showMovieInfoForId.value = id;
 }
 
 const getImageUrl = function () {
-    return props.movie['cover_image'] === ' '? '/images/no_image.png' : props.movie['cover_image'];
+    return props.movie['cover_image'] === ' ' ? '/images/no_image.png' : props.movie['cover_image'];
+}
+
+const watchListButtonClicked = () => {
+    let status = toggleFromWatchList(props.movie);
+
+    watch(status, (status) => {
+        if (status.success) {
+            watchListHas.value = !watchListHas.value
+            toast.add({severity: 'Success', summary: status.summary, detail: status.details, life: 3000});
+        }
+    })
 }
 </script>
 
@@ -28,8 +58,14 @@ const getImageUrl = function () {
             :alt="'cover image of movie ' + movie['name']"
             :class="['w-[12.5rem] h-[18rem] ' + widthClass + ' ' + heightClass, {'scale-125 transition ': showMovieInfoForId === movie['id']}]"
         >
-        <div class="absolute right-0 top-0 z-20 text-black bg-white font-extrabold">
-            <span class="p-1 text-xs [@6rem]:text-sm">{{ movie['rating'] }}</span>
+        <div
+            :class="['text-xs @[6rem]:text-sm absolute w-full flex justify-between items-start right-0 top-0 z-20 text-black font-extrabold', {'!justify-end': !showWatchListButton}]">
+            <span class="p-1 bg-white">{{ (movie['rating']).toFixed(1) }}</span>
+            <i
+                v-if="showWatchListButton"
+                @click.prevent="watchListButtonClicked"
+                :class="['pi p-1 @[6rem]:p-2 bg-white', {'pi-heart': !watchListHas, 'pi-heart-fill': watchListHas}]"
+            />
         </div>
         <div
             v-if="showMovieInfoForId === movie['id']"
---
+++ resources/js/Composables/useToggleMovieFromWatchList.js
@@ -0,0 +1,53 @@
+import {useForm} from "@inertiajs/vue3";
+import {reactive, ref} from "vue";
+
+const watchList = ref([]);
+
+export function useToggleMovieFromWatchList(watchListIds) {
+    watchList.value = watchListIds;
+    return {checkWatchListHas, toggleFromWatchList};
+}
+
+const checkWatchListHas = (movieId) => {
+    return watchList.value.includes(movieId);
+};
+
+const toggleFromWatchList = (movie) => {
+    const status = reactive({
+        success: false,
+        summary: '',
+        details: '',
+    });
+
+    useForm({
+        'name': movie.name,
+        'imdb_id': movie.imdb_code,
+        'yts_id': movie.id,
+        'image': movie.cover_image,
+        'genres': movie.genres,
+        'released_date': movie.year?.toString()
+    }).post(route('movies-watch-list.store'), {
+        onSuccess: () => {
+            let index = watchList.value.indexOf(movie.id);
+
+            if (index === -1) {
+                watchList.value.push(movie.id)
+            } else {
+                watchList.value.splice(index, 1)
+            }
+
+            let summary = 'Movie Added';
+            let details = 'Movie added to the watch list';
+
+            if (! checkWatchListHas(movie.id)) {
+                summary = 'Movie Removed';
+                details = 'Movie removed from watch list';
+            }
+            status.success = true;
+            status.summary = summary;
+            status.details = details;
+        }
+    });
+
+    return status;
+};
