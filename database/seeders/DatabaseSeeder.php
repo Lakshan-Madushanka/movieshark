9d92ab436cf0e36c6bd6c826e7eae4408e2225dd
Lakshan Madushanka <epmadushanka@gmail.com>
2024-05-16 10:14:24 +0530

    Fix DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
 
         $this->call([
-            WatchListSeeder::class,
+            MovieSeeder::class,
         ]);
     }
 }
