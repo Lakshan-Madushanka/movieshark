8a84496f6f1f06e06083e68337c0efeea053f026
Lakshan Madushanka <epmadushanka@gmail.com>
2024-05-16 13:56:13 +0530

    Fix profile page invisible issue

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -21,7 +21,7 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         ref="input"
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="border-gray-300 focus:border-indigo-500 text-black focus:ring-indigo-500 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
     >
