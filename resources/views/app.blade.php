a2d1f707388dad804dcec54b108801a51e4ec0c3
Lakshan Madushanka <epmadushanka@gmail.com>
2024-12-10 04:13:57 +0530

    Add favicon and icon path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" type="image/x-icon" href="/images/logo.avif">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
