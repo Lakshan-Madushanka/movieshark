4e4bea463412bdca10d2937c4a85f3deffc03eed
Lakshan Madushanka <epmadushanka@gmail.com>
2024-12-21 00:24:21 +0530

    Add favicon routes to vercel.json

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,8 +8,6 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
